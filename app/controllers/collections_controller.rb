4b51cc9eb63db1e30a717841c3fc64e5e650dbfb
Joberman055 <jacksonoberman@gmail.com>
2023-11-17 15:09:44 -0800

    Update nav and collection/sets view

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -3,5 +3,8 @@ class CollectionsController < ApplicationController
     if current_user.blank?
       redirect_to root_path
     end
+
+    @user = User.find(params[:user_id])
+    @set = @user.col_set.first
   end
 end
