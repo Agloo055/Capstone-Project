faab4aec1beb5cc82e9b8b2dbb8885ad0a1144ac
Joberman055 <jacksonoberman@gmail.com>
2023-11-16 17:11:09 -0800

    Make set card partial and add it to collection sets index

--- app/controllers/col_sets_controller.rb
+++ app/controllers/col_sets_controller.rb
@@ -1,4 +1,25 @@
 class ColSetsController < ApplicationController
   def index
+    if current_user.blank?
+      redirect_to root_path
+    end
+    user = User.find(current_user.id)
+    @sets = user.col_set
   end
+
+  def show
+    
+  end
+
+  def create
+    puts params
+    @user = User.find(current_user.id)
+    @set = @user.col_set.create(col_set_params)
+    redirect_to user_collections_path
+  end
+
+  private
+    def col_set_params
+      params.require(:col_set).permit(:set_num, :name, :year, :theme_id, :num_parts, :set_img_url, :set_url)
+    end
 end
